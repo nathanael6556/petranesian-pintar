2802e68ab9491ccca7389ed5aa9ada7d1fe8abde
Nathanael Tjandra <nathanael6556@gmail.com>
2024-10-14 15:21:29 +0000

    Change whispercpp python binding

--- transcribe-cpp.py
+++ transcribe-cpp.py
@@ -1,8 +1,26 @@
-from whispercpp import Whisper
+import os
+from os.path import splitext, abspath
+from whisper_cpp_python import Whisper
+from whisper_cpp_python.whisper_cpp import whisper_progress_callback
 
 
-w = Whisper.from_pretrained("/home/jupyter-c14220202@john.pet-104c4/petranesian-pintar/src/samples/ggml-large-v3.bin")
+INPUT_PATH = abspath("./samples/Lecture04/DS-Lct04-Communication.mp3")
+MODEL_PATH = "../ggml-small.bin"
 
-result = w.transcribe_from_file("./DS Lect04 Communication.mp3")
-with open("transcript.txt", "w") as f:
-    f.write(result)
+
+# Convert audio to WAV
+if not INPUT_PATH.endswith(".wav"):
+    output_path, _ = splitext(INPUT_PATH)
+    output_path += ".wav"
+    status = os.system(f"ffmpeg -i '{INPUT_PATH}' -ar 16000 -ac 1 -c:a pcm_s16le {output_path}")
+else:
+    output_path = INPUT_PATH
+
+
+def callback(ctx, state, i, p):
+    print(i)
+
+model = Whisper(MODEL_PATH)
+model.params.progress_callback = whisper_progress_callback(callback)
+
+print(model.transcribe(output_path))
