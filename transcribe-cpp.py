445a529002ebc66b70730d9b9b7d695aaf1ac5e8
Nathanael Tjandra <nathanael6556@gmail.com>
2024-10-14 15:35:58 +0000

    Fix transcript language and save to file

--- transcribe-cpp.py
+++ transcribe-cpp.py
@@ -4,23 +4,28 @@ from whisper_cpp_python import Whisper
 from whisper_cpp_python.whisper_cpp import whisper_progress_callback
 
 
-INPUT_PATH = abspath("./samples/Lecture04/DS-Lct04-Communication.mp3")
+RAW_INPUT_PATH = abspath("./samples/Lecture04/DS-Lct04-Communication.mp3")
+OUTPUT_PATH = "transcript.txt"
 MODEL_PATH = "../ggml-small.bin"
 
 
 # Convert audio to WAV
-if not INPUT_PATH.endswith(".wav"):
-    output_path, _ = splitext(INPUT_PATH)
-    output_path += ".wav"
-    status = os.system(f"ffmpeg -i '{INPUT_PATH}' -ar 16000 -ac 1 -c:a pcm_s16le {output_path}")
+if not RAW_INPUT_PATH.endswith(".wav"):
+    input_path, _ = splitext(RAW_INPUT_PATH)
+    input_path += ".wav"
+    status = os.system(f"ffmpeg -i '{RAW_INPUT_PATH}' -ar 16000 -ac 1 -c:a pcm_s16le {input_path}")
 else:
-    output_path = INPUT_PATH
+    input_path = RAW_INPUT_PATH
 
 
 def callback(ctx, state, i, p):
     print(i)
 
+
 model = Whisper(MODEL_PATH)
 model.params.progress_callback = whisper_progress_callback(callback)
 
-print(model.transcribe(output_path))
+transcript = model.transcribe(input_path, response_format='text', language='id')
+print(transcript)
+with open(OUTPUT_PATH, "w") as f:
+    f.write(transcript)
