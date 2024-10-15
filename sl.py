53970e7fb536bd8806a7b4d5dad8ac2995fce698
Nathan Adhitya <NathanAdhitya@users.noreply.github.com>
2024-10-15 20:09:45 +0700

    add function

--- sl.py
+++ sl.py
@@ -21,7 +21,7 @@ import re
 # Prepare Environment
 load_dotenv()
 nest_asyncio.apply()
-logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # Base Settings
@@ -29,18 +29,20 @@ model = "llama3.1:8b-instruct-q4_0"
 embed_model = "mxbai-embed-large:latest"
 ollama_endpoint = "http://127.0.0.1:11434"
 
-c = Client(ollama_endpoint, timeout=60)
+c = Client(ollama_endpoint, timeout=300)
 Settings.llm = Ollama(model=model, base_url=ollama_endpoint)
 Settings.embed_model = OllamaEmbedding(base_url=ollama_endpoint, model_name=embed_model)
 
 # Summariser
-summariser_splitter = SentenceSplitter(chunk_size=15*1024, chunk_overlap=512)
+summariser_splitter = SentenceSplitter(chunk_size=15 * 1024, chunk_overlap=512)
+
+
 def summarise(d: Document) -> str:
     for chunk in summariser_splitter.split_text(d.text):
         result = c.generate(
             model=model,
             prompt=(
-    f"""Clean up the given document and restructure it using Markdown.
+                f"""Clean up the given document and restructure it using Markdown.
 Clean up the document by structuring the information into points.
 Fix and correct grammatical and language errors.
 You must include all crucial information. You must not add any information that is not in the document.
@@ -53,17 +55,20 @@ You must include all crucial information. You must not add any information that 
             ),
             options={
                 "temperature": 0,
-                "num_ctx": 16*1024,
-                "num_predict": 16*1024,
-            }
+                "num_ctx": 16 * 1024,
+                "num_predict": 2 * 1024,
+            },
         )
 
     response = result["response"].strip()
-    logging.info(f"""
+    logging.info(
+        f"""
 [Summary]
 {response}
-""")
+"""
+    )
     return response
+
 
 # Create questions from summary
 def derive_questions(summary: str) -> list[str]:
@@ -76,7 +81,7 @@ def derive_questions(summary: str) -> list[str]:
     result = c.generate(
         model=model,
         prompt=(
-    f"""Create 5 questions that covers this summary.
+            f"""Create 5 questions that covers this summary.
 The questions should ask reasoning and not ask for facts.
 You must answer in the following format:
 <Questions>
@@ -91,8 +96,8 @@ You must answer in the following format:
         ),
         options={
             "temperature": 1,
-            "num_predict": 1*1024,
-        }
+            "num_predict": 1 * 1024,
+        },
     )
 
     response = result["response"]
@@ -101,3 +106,24 @@ You must answer in the following format:
     questions = [q.strip() for q in questions]
 
     return questions
+
+
+# Create questions from materials
+def derive_questions_from_materials(documents: list[Document]) -> tuple:
+    # Get the summaries
+    summaries = map(summarise, documents)
+
+    # Create questions
+    questions = derive_questions("\n".join(summaries))
+
+    return (summaries, questions)
+
+
+documents: list[Document] = []
+
+for file in ["samples/Lecture04/transcribe.txt", "samples/Lecture04/pdf_text.txt"]:
+    with open(file, "r", errors="ignore") as f:
+        text = f.read()
+        documents.append(Document(text=text))
+
+print(derive_questions_from_materials(documents))
